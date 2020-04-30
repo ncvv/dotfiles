2a8aaee86f9a6affb14bd5461be5985565fd660a
Nicolas Wipfler <ncvv@users.noreply.github.com>
2020-04-30 19:50:24 +0200

    Update setup.

--- .dotfiles/setup.sh
+++ .dotfiles/setup.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-#if [[ $OSTYPE == "linux-gnu" ]]; then
-    sudo apt-get install rsync
-#elif [[ $OSTYPE == "darwin" ]]; then
-    # Install brew
- #   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    # Install dependencies
-  #  brew install git rsync
-#fi
-
+# Install dependencies of this setup
 sudo apt-get install rsync
 
 # Clone dotfiles
@@ -16,8 +8,7 @@ mkdir .dotfiles
 git clone --separate-git-dir=$HOME/.dotfiles/.git https://github.com/ncvv/dotfiles.git .tmp
 rsync --recursive --verbose --exclude '.git' .tmp/ $HOME/
 rm -rf .tmp
-# Don't show untracked files in dotfiles repository $HOME
-#dotfiles config --local status.showUntrackedFiles no
+
 # Install everything else
-#chmod +x .install-dotfiles.sh
-#./.install-dotfiles.sh
+chmod +x .dotfiles/install.sh
+.dotfiles/install.sh
